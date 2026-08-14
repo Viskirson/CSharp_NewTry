66c519d9ca057fd5ee3e28e5c8a9639f157945f7
aleksey terekhin <kurosagi123@gmail.com>
2026-08-14 18:15:45 +0300

    Первые потуги.

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет мир, это моя попытка учить C#!");
+//Пара переменных до кучи.
+int magicNumber = 42;
+string magicString = "42";
+bool notMagicBool = false;
