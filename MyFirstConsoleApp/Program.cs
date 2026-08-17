b37b33be4c65fd1732f52c95318650dfe749f1cd
aleksey terekhin <kurosagi123@gmail.com>
2026-08-17 21:32:14 +0300

    Добавление кода с использованием операторов

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -6,8 +6,34 @@ bool notMagicBool = false;
 
 void OperatorExample(string someSrt)
 {
-    Console.WriteLine(someSrt);
+    Console.WriteLine(someSrt + " Это магическое число!");
+    int width = 3, height = 3, area;
+    Console.WriteLine(3+width++); //Постфиксны инкремент
+    Console.WriteLine(width);
+    Console.WriteLine(3+(++height)); //Перфиксный инкремент
+    Console.WriteLine(height);
+
+    area = width * height;
+    Console.WriteLine($"\nВысота:\t{height}.\nШирина:\t{width}.\n" +
+        $"Площадь: {area}.\n" +
+        $"{notMagicBool}");
+
+    width = 3;
+    width++;
+
+    height = 2 + 4;
+    area = width * height;
+    Console.WriteLine(area);
+
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    bool trueValue = true;
+    Console.WriteLine(trueValue);
 }
 
+
+
+
 OperatorExample(magicString);
-Console.ReadLine();
